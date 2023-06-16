cb7e7b65619498a4f7dd16ce24354038347ce793
Kevin Cain <kevin_cain@hotmail.co.uk>
2023-06-16 15:05:25 +0100

    unit tests are now passing

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -1,8 +1,30 @@
+import {useState} from 'react';
+
 const NewMessageForm = () => {
+    const [inputText, setInputText] = useState('');
+
+    const handleChange = (event) => {
+        setInputText(event.target.value);
+    }
+
+    const handleClick = () => {
+        setInputText('');
+    }
+
     return (
         <>
-            <input type="text" data-testid='messageText' />
-            <button data-testid='sendButton'>Send</button>
+            <input 
+                type="text"
+                data-testid='messageText'
+                value={inputText}
+                onChange={handleChange}
+            />
+            <button 
+                data-testid='sendButton'
+                onClick={handleClick}
+            >
+                Send
+            </button>
         </>
     )
 }
--- src/NewMessageForm.test.js
+++ src/NewMessageForm.test.js
@@ -2,18 +2,23 @@ import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import NewMessageForm from './NewMessageForm';
 
-const getForm = () => {
-    render(<NewMessageForm />);
-}
-
-describe('<NewMessageForm />', () => {  
-    it('clears the text field', async () => {
-    getForm ();
-
-    await userEvent.type(screen.getByTestId('messageText'), 'New message');
-
-    userEvent.click(screen.getByTestId('sendButton'));
-
-    expect(screen.getByTestId('messageText').value).toEqual('');
+describe('<NewMessageForm />', () => {
+    describe('clicking the send button', () => {
+      async function sendMessage() {
+        render(<NewMessageForm />);
+  
+        await userEvent.type(
+          screen.getByTestId('messageText'),
+          'New message',
+        );
+        userEvent.click(screen.getByTestId('sendButton'));
+      }
+  
+      it('clears the text field', async () => {
+        await sendMessage();
+        expect(screen.getByTestId('messageText').value).toEqual('');
+      });
     });
-})
+  });
+  
+  
